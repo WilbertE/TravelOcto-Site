92bcd20698d9233ae6289521f1dfe4b5c0b880c8
Wilbert van den Esker <wilbert@spearpoint.nl>
2020-11-27 02:13:27 +0100

    Added TradeTracker Code

--- pages/_document.js
+++ pages/_document.js
@@ -81,6 +81,12 @@ export default class Document extends NextDocument {
           `,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: ` var _TradeTrackerTagOptions = { t: 'a', s: '370553', chk: '92548de6aaf982a9fcae4b127efda7e7', overrideOptions: {} };
+            (function() {var tt = document.createElement('script'), s = document.getElementsByTagName('script')[0]; tt.setAttribute('type', 'text/javascript'); tt.setAttribute('src', (document.location.protocol == 'https:' ? 'https' : 'http') + '://tm.tradetracker.net/tag?t=' + _TradeTrackerTagOptions.t + '&amp;s=' + _TradeTrackerTagOptions.s + '&amp;chk=' + _TradeTrackerTagOptions.chk); s.parentNode.insertBefore(tt, s);})();`,
+            }}
+          />
         </Head>
         <body>
           <Main />
